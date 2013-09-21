51a6c97df473d76032a00ffd9a4ddb4a28871b21
Charley Hutchison <glenwayguy@gmail.com>
2013-09-21 03:50:33 -0400

    Added Issue Model and Issue View Controller

---
+++ SoapBox/SoapBox/Issue.m
@@ -0,0 +1,13 @@
+//
+//  Issue.m
+//  SoapBox
+//
+//  Created by Charley Hutchison on 9/21/13.
+//  Copyright (c) 2013 Josh. All rights reserved.
+//
+
+#import "Issue.h"
+
+@implementation Issue
+
+@end
