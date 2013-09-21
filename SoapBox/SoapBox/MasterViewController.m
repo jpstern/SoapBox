0a3e9a1c0e65d1ac716790dc3f07befb35304ab3
Josh Stern <jpstern@umich.edu>
2013-09-21 01:28:56 -0400

    dfafs

---
+++ SoapBox/SoapBox/MasterViewController.m
@@ -0,0 +1,38 @@
+//
+//  MasterViewController.m
+//  SoapBox
+//
+//  Created by Josh Stern on 9/21/13.
+//  Copyright (c) 2013 Josh. All rights reserved.
+//
+
+#import "MasterViewController.h"
+
+@interface MasterViewController ()
+
+@end
+
+@implementation MasterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
