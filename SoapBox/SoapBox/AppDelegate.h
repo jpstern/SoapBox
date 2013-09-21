352a77636d472eeac536b3612a3fb5e4f9120ca7
Josh Stern <jpstern@umich.edu>
2013-09-21 00:15:47 -0400

    Adding Xcode project to repo

---
+++ SoapBox/SoapBox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SoapBox
+//
+//  Created by Josh Stern on 9/21/13.
+//  Copyright (c) 2013 Josh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SoapBox/SoapBox/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SoapBox
+//
+//  Created by Josh Stern on 9/21/13.
+//  Copyright (c) 2013 Josh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
