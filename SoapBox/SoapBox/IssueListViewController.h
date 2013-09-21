5251aa8ec8b8d7cd458bfcadac2a8807ea057c60
Charley Hutchison <glenwayguy@gmail.com>
2013-09-21 09:21:13 -0400

    Created the IssuesListViewControllers, and animated the status bar from the container-thingy.

---
+++ SoapBox/SoapBox/IssueListViewController.h
@@ -0,0 +1,13 @@
+//
+//  IssueListViewController.h
+//  SoapBox
+//
+//  Created by Charley Hutchison on 9/21/13.
+//  Copyright (c) 2013 Josh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IssueListViewController : UITableViewController
+
+@end
