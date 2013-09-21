52684fef34768053b268fc8045628f54fb1a1dde
Josh Stern <jpstern@umich.edu>
2013-09-21 00:20:30 -0400

    initial project setup, removed storyboard and added new nib

--- SoapBox/SoapBox/AppDelegate.m
+++ SoapBox/SoapBox/AppDelegate.m
@@ -8,10 +8,19 @@
 
 #import "AppDelegate.h"
 
+#import "ViewController.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    ViewController *viewController = [[ViewController alloc] init];
+    
+    self.window.rootViewController = viewController;
+    [self.window makeKeyAndVisible];
+    
     // Override point for customization after application launch.
     return YES;
 }
