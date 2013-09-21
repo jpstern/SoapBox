1c9a2fbe51cc1ab8822abe9f226d221a37d24e66
Jackson <jessupjn@umich.edu>
2013-09-21 05:01:51 -0400

    updates, beginnings of issue list controller

---
+++ SoapBox/SoapBox/IssuesListViewController.h
@@ -0,0 +1,13 @@
+//
+//  IssuesListViewController.h
+//  SoapBox
+//
+//  Created by Jackson on 9/21/13.
+//  Copyright (c) 2013 Josh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IssuesListViewController : UIViewController
+
+@end
