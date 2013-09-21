185a9c2eed6ef7da75f2bdd1ef5a0705a7922935
Jackson <jessupjn@umich.edu>
2013-09-21 01:18:51 -0400

    added table view (for behind map)

---
+++ SoapBox/SoapBox/Table.h
@@ -0,0 +1,17 @@
+//
+//  Table.h
+//  SoapBox
+//
+//  Created by Jackson on 9/21/13.
+//  Copyright (c) 2013 Josh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Table : UIViewController {
+  IBOutlet UITableView *mainMenu;
+  
+  
+}
+
+@end
